6c310b3aff1eab75e7cb596f3c24f9a5fe6f2f0e
Silvio Henrique Ferreira <shferreira@me.com>
2017-10-19 21:56:19 -0200

    Echo

--- app.go
+++ app.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo"
+	"html/template"
 )
 
 type User struct {
@@ -13,16 +14,18 @@ type User struct {
 
 func main() {
 	db, _ := gorm.Open("mysql", "root@/test")
+	t := template.Must(template.ParseGlob("views/*.html"))
 
-	r := gin.Default()
-	r.LoadHTMLGlob("views/*")
-	r.GET("/hello/:name", func(c *gin.Context) {
-		c.HTML(200, "index.html", &struct{ Name string }{c.Param("name")})
+	e := echo.New()
+	e.GET("/hello/:name", func(c echo.Context) error {
+		t.ExecuteTemplate(c.Response(), "index.html", &struct{ Name string }{c.Param("name")})
+		return nil
 	})
-	r.GET("/find/:name", func(c *gin.Context) {
+	e.GET("/find/:name", func(c echo.Context) error {
 		result := User{}
 		db.First(&result)
-		c.HTML(200, "index.html", &struct{ Name string }{result.Name})
+		t.ExecuteTemplate(c.Response(), "index.html", &struct{ Name string }{result.Name})
+		return nil
 	})
-	r.Run()
+	e.Start(":8080")
 }
