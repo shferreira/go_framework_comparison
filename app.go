ba3e3dc7e934c7a8ed6da1a4d9d4ac50423b0500
Silvio Henrique Ferreira <shferreira@me.com>
2017-10-21 19:02:40 -0200

    Echo + QuickTemplate

--- app.go
+++ app.go
@@ -1,10 +1,11 @@
+//go:generate qtc -dir=views
 package main
 
 import (
+	"./views"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
-	"html/template"
 )
 
 type User struct {
@@ -14,17 +15,16 @@ type User struct {
 
 func main() {
 	db, _ := gorm.Open("mysql", "root@/test")
-	t := template.Must(template.ParseGlob("views/*.html"))
 
 	e := echo.New()
 	e.GET("/hello/:name", func(c echo.Context) error {
-		t.ExecuteTemplate(c.Response(), "index.html", &struct{ Name string }{c.Param("name")})
+		views.WritePageTemplate(c.Response(), &views.Index{c.Param("name")})
 		return nil
 	})
 	e.GET("/find/:name", func(c echo.Context) error {
 		result := User{}
 		db.First(&result)
-		t.ExecuteTemplate(c.Response(), "index.html", &struct{ Name string }{result.Name})
+		views.WritePageTemplate(c.Response(), &views.Index{result.Name})
 		return nil
 	})
 	e.Start(":8080")
