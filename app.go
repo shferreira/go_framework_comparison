35f4640329e982d482b2ee5947aee8821b5b6e79
Silvio Henrique Ferreira <shferreira@me.com>
2017-10-22 16:28:01 -0200

    Echo + Ego

--- app.go
+++ app.go
@@ -1,10 +1,11 @@
+//go:generate ego views
 package main
 
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
-	"html/template"
+	"./views"
 )
 
 type User struct {
@@ -14,18 +15,15 @@ type User struct {
 
 func main() {
 	db, _ := gorm.Open("mysql", "root@/test")
-	t := template.Must(template.ParseGlob("views/*.html"))
 
 	e := echo.New()
 	e.GET("/hello/:name", func(c echo.Context) error {
-		t.ExecuteTemplate(c.Response(), "index.html", &struct{ Name string }{c.Param("name")})
-		return nil
+		return views.Index(c.Response(), c.Param("name"))
 	})
 	e.GET("/find/:name", func(c echo.Context) error {
 		result := User{}
 		db.First(&result)
-		t.ExecuteTemplate(c.Response(), "index.html", &struct{ Name string }{result.Name})
-		return nil
+		return views.Index(c.Response(), result.Name)
 	})
 	e.Start(":8080")
 }
