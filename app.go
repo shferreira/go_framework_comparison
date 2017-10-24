8cd4aadc4e973b5d280fd216e45e32df311c38f5
Silvio Henrique Ferreira <shferreira@me.com>
2017-10-23 23:10:19 -0200

    Echo + JSON

--- app.go
+++ app.go
@@ -1,32 +1,26 @@
 package main
 
 import (
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
-	"html/template"
 )
 
 type User struct {
-	Id   int
-	Name string
+	Id   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 func main() {
-	db, _ := gorm.Open("mysql", "root@/test")
-	t := template.Must(template.ParseGlob("views/*.html"))
+	users := map[int]*User{}
 
 	e := echo.New()
-	e.GET("/hello/:name", func(c echo.Context) error {
-		t.ExecuteTemplate(c.Response(), "index.html", &struct{ Name string }{c.Param("name")})
-		return nil
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(200, users)
 	})
-	e.GET("/find/:name", func(c echo.Context) error {
-		result := User{}
-		db.First(&result)
-		t.ExecuteTemplate(c.Response(), "index.html", &struct{ Name string }{result.Name})
-		return nil
+	e.PUT("/:id", func(c echo.Context) error {
+		user := &User{}
+		c.Bind(user)
+		users[user.Id] = user
+		return c.JSON(200, user)
 	})
-	e.Static("/", "public")
 	e.Start(":8080")
 }
