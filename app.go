283f7e8c067bb6622eecf502c00a27faa684c1c8
Silvio Henrique Ferreira <shferreira@users.noreply.github.com>
2017-12-08 23:18:11 -0200

    Add CSRF protection

--- app.go
+++ app.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"golang.org/x/oauth2"
-	"io/ioutil"
-	"net/http"
+	"golang.org/x/oauth2/facebook"
 	"os"
 )
 
@@ -12,32 +12,27 @@ func main() {
 	facebook := &oauth2.Config{
 		ClientID:     os.Getenv("FACEBOOK_KEY"),
 		ClientSecret: os.Getenv("FACEBOOK_SECRET"),
+		Endpoint: facebook.Endpoint,
 		RedirectURL:  "http://localhost:8080/auth/facebook/callback",
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://www.facebook.com/dialog/oauth",
-			TokenURL: "https://graph.facebook.com/oauth/access_token",
-		},
 		Scopes: []string{
 			"email",
 		},
 	}
 
 	e := echo.New()
+	e.Use(middleware.CSRF())
 	e.GET("/auth/:provider", func(c echo.Context) error {
-		return c.Redirect(302, facebook.AuthCodeURL("tmp"))
+		return c.Redirect(302, facebook.AuthCodeURL(c.Get("csrf").(string)))
 	})
 	e.GET("/auth/:provider/callback", func(c echo.Context) error {
+		if c.QueryParam("state") != c.Get("csrf").(string) {
+			return c.String(403, "CSRF detected")
+		}
 		token, err := facebook.Exchange(oauth2.NoContext, c.QueryParam("code"))
-		if err != nil || !token.Valid() {
+		if err != nil {
 			return c.String(403, "Invalid token")
 		}
-		res, err := http.Get("https://graph.facebook.com/me?fields=email,first_name,last_name,link,about,id,name,picture,location&access_token=" + token.AccessToken)
-		if err != nil {
-			return c.String(403, "Can't read user data")
-		}
-		body, _ := ioutil.ReadAll(res.Body)
-
-		return c.String(200, string(body))
+		return c.String(200, token.AccessToken)
 	})
 	e.Start(":8080")
 }
