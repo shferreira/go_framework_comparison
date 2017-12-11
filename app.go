499a27a84e2507aa3d68128eff30f91cf546aa9c
Silvio Henrique Ferreira <shferreira@users.noreply.github.com>
2017-12-11 14:36:47 -0200

    Update app.go

--- app.go
+++ app.go
@@ -4,7 +4,6 @@ import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/facebook"
 	"os"
 )
 
@@ -12,8 +11,11 @@ func main() {
 	facebook := &oauth2.Config{
 		ClientID:     os.Getenv("FACEBOOK_KEY"),
 		ClientSecret: os.Getenv("FACEBOOK_SECRET"),
-		Endpoint: facebook.Endpoint,
-		RedirectURL:  "http://localhost:8080/auth/facebook/callback",
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://www.facebook.com/dialog/oauth",
+			TokenURL: "https://graph.facebook.com/oauth/access_token",
+		},
+		RedirectURL: "http://localhost:8080/auth/facebook/callback",
 		Scopes: []string{
 			"email",
 		},
@@ -21,10 +23,10 @@ func main() {
 
 	e := echo.New()
 	e.Use(middleware.CSRF())
-	e.GET("/auth/:provider", func(c echo.Context) error {
+	e.GET("/auth/facebook", func(c echo.Context) error {
 		return c.Redirect(302, facebook.AuthCodeURL(c.Get("csrf").(string)))
 	})
-	e.GET("/auth/:provider/callback", func(c echo.Context) error {
+	e.GET("/auth/facebook/callback", func(c echo.Context) error {
 		token, err := facebook.Exchange(oauth2.NoContext, c.QueryParam("code"))
 		if err != nil || !token.Valid() || c.QueryParam("state") != c.Cookie("_csrf") {
 			return c.String(403, "Invalid token")
